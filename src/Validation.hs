a470af259e8bf3022a9a534973ecbf72e092b6b9
Tero Laitinen <tero.laitinen@feriko.fi>
2013-05-15 14:35:25 +0300

    YesodAuthPersist for YesodSubDispatch

--- src/Validation.hs
+++ src/Validation.hs
@@ -6,8 +6,13 @@ import Validation.Names
 import Validation.Refs
 import Validation.Fields
 import Validation.Handlers
+import Data.Maybe
+
 validate :: Module -> String
 validate m = nameErrors ns ++ refErrors ns (refs m) ++ fieldErrors m ns 
            ++ handlerErrors m
-           
+           ++ missingModuleName 
     where ns = names m
+          missingModuleName 
+            | isNothing $ modName m = "Top-level module missing name"
+            | otherwise = ""
