3c785496bd63f77184396a2bfaed0ff340ef8c34
Tero Laitinen <tero.laitinen@iki.fi>
2013-05-24 10:34:08 +0300

    removed -i parameter

--- src/main.hs
+++ src/main.hs
@@ -17,34 +17,20 @@ data Flag = Input FilePath
 
 options :: [OptDescr Flag]
 options = [
-    Option ['i'] ["input"]  (ReqArg Input "FILE")      "read Yesod DSL definitions from FILE"
   ]
 
-data Opts = Opts {
-    optInput      :: Maybe FilePath
-}
-
-foldOptions :: [Flag] -> Opts
-foldOptions fs = foldl f (Opts Nothing) fs
-    where f o (Input i) = o { optInput = Just i }
-
-checkOptions :: Opts -> Opts
-checkOptions o
-    | isNothing $ optInput o = error $ "missing -i parameter\n" ++ usageInfo header options
-    | otherwise = o
-
-header = "Usage: yesod-dsl [OPTION...]"
+header = "Usage: yesod-dsl FILE"
 main = do
     args <- getArgs
     case getOpt RequireOrder options args of
-        (flags, [],      [])     -> main' $ checkOptions $ foldOptions flags
-        (_,     nonOpts, [])     -> error $ "unrecognized arguments: " ++ unwords nonOpts
+        (_,     (path:_), [])     -> main' path
         (_,     _,       msgs)   -> error $ concat msgs ++ usageInfo header options
+
 
     where
                          
-        main' o = do
-            dbs <- parse (fromJust $ optInput o)
+        main' path = do
+            dbs <- parse path
             let ast  = implementClasses . mergeModules $ dbs
             let errors = validate ast
             if null errors 
