9b9dec6d6601f5d24754d6fa798bd97d2b29f98a
Tero Laitinen <tero.laitinen@iki.fi>
2013-07-16 11:37:18 +0300

    enum values are now prefixed with enum name, custom Read and Show instances for enums

--- src/Generator/Models.hs
+++ src/Generator/Models.hs
@@ -35,6 +35,12 @@ enum e = T.unpack $(codegenFile "codegen/enum.cg")
           toPathPiece v = T.unpack $(codegenFile "codegen/enum-topathpiece.cg")
           parseJSON v = T.unpack $(codegenFile "codegen/enum-parsejson.cg")
           toJSON v = T.unpack $(codegenFile "codegen/enum-tojson.cg")
+          readsPrecs = concatMap readsPrec (enumValues e)
+          showsPrecs = concatMap showsPrec (enumValues e)
+          readsPrec v = T.unpack $(codegenFile "codegen/enum-readsprec.cg")
+          showsPrec v = T.unpack $(codegenFile "codegen/enum-showsprec.cg")
+          toCharList s = "'" ++ (intercalate "':'" (map (:[]) s)) ++ "'"
+          prefixedValues e = intercalate " | " $ map ((enumName e) ++) $ enumValues e
 
 modelField :: Field -> String
 modelField f = T.unpack $(codegenFile "codegen/model-field.cg")
