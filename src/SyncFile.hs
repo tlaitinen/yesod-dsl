e1e3ff98c6ab79119c68ef6b8d418f7ef4594647
Tero Laitinen <tero.laitinen@iki.fi>
2014-02-05 10:57:21 +0200

    fixed type ambiguity bug, issue #2

--- src/SyncFile.hs
+++ src/SyncFile.hs
@@ -2,10 +2,11 @@ module SyncFile where
 import Prelude hiding (readFile)
 import System.IO (FilePath, writeFile)
 import System.IO.Strict (readFile)
+import Control.Exception (catch, SomeException)
 
 syncFile :: FilePath -> String -> IO ()
 syncFile path content = do
-    oldContent <- catch (readFile path) (\_ -> return "")
+    oldContent <- catch (readFile path) ((\_ -> return "") :: SomeException -> IO String)
     if content /= oldContent
         then do
             putStrLn $ "Updating " ++ path
