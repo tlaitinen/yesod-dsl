95678f87a4dce6fb6682de6b1d3a589ab72f5f95
Tero Laitinen <tero.laitinen@iki.fi>
2014-02-18 11:10:15 +0200

    just-function parameter in parenthesis in generated code

--- src/Generator/Common.hs
+++ src/Generator/Common.hs
@@ -4,7 +4,7 @@ maybeHsJust True v = "(Just " ++ v ++ ")"
 maybeHsJust False v = v
 makeJust :: Int -> String -> String
 makeJust n t 
-    | n > 0 = "(just " ++ makeJust (n - 1) t ++ ")"
+    | n > 0 = "(just (" ++ makeJust (n - 1) t ++ "))"
     | otherwise = t
 
 
