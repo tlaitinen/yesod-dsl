8c2ba060e86bedd9ef57da8444c46788a7073289
Tero Laitinen <tero.laitinen@iki.fi>
2014-06-24 13:04:59 +0300

    prototyping parsec-based context-aware parser

---
+++ src/YesodDsl/Parser/Tokens.hs
@@ -0,0 +1,8 @@
+module YesodDsl.Parser.Tokens where
+
+import YesodDsl.Parser.Types
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec
+
+
---
+++ src/YesodDsl/Parser/Types.hs
@@ -0,0 +1,33 @@
+module YesodDsl.Parser.Types where
+
+
+type SymName = String
+
+
+
+
+data SymContent = SymEntity EntityName
+data SymScope = SymScope
+data Sym = Sym {
+    symName    :: String,
+    symScope   :: SymScope,
+    symContent :: SymContent
+}
+
+
+data Env = Env {
+    envSymbols :: [Sym],
+    envDefs    :: [ModDef],
+    envFiles   :: [FilePath]
+}
+
+
+type YdslParser a = IndentParserT String Env IO a
+
+initEnv :: FilePath -> Env
+initEnv path = Env {
+    envSymbols = [],
+    envDefs    = [],
+    envFiles   = [path]
+}
+
