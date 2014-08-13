2ed89df90c0b620186063efdb36380803d48c154
Tero Laitinen <tero.laitinen@iki.fi>
2014-08-13 23:46:37 +0300

    missing main.hs file added

---
+++ src/main/main.hs
@@ -0,0 +1,46 @@
+import YesodDsl.Lexer
+import YesodDsl.Parser
+import System.Environment
+import System.Console.GetOpt
+import YesodDsl.ModuleMerger
+import YesodDsl.Validation
+import YesodDsl.ClassImplementer
+import YesodDsl.Generator
+import Control.Monad
+import System.IO
+import YesodDsl.ExpandMacros
+
+data Flag = 
+          FayPath String deriving Eq
+options :: [OptDescr Flag]
+options = [
+    Option ['f']     ["fay"] (ReqArg FayPath "FILE") "translate DSL definition to Fay compatible code and store it in FILE"
+  ]
+
+header :: String
+
+header = "Usage: yesod-dsl FILE"
+main :: IO ()
+main = do
+    args <- getArgs
+    case getOpt RequireOrder options args of
+        (o,     (path:_), [])     -> main' o path
+        (_,     _,       msgs)   -> error $ concat msgs ++ usageInfo header options
+
+
+    where
+                         
+        main' o path = do
+            mast <- parse path
+            case mast of
+                Just ast -> do
+                    let errors = validate ast
+                    if null errors 
+                        then do
+                            generate path $ ast 
+                            forM_ o (processFlag ast)
+                        else hPutStrLn stderr errors
+                Nothing -> return ()
+        processFlag ast (FayPath path) = genFay path ast
+            
+        processFlag _ _ = return ()
