69505d0dc246707c79a3c3351bacda6225539a20
Tero Laitinen <tero.laitinen@iki.fi>
2014-09-19 13:35:57 +0300

    YesodDSL.Validation.State dropped

--- src/YesodDsl/Validation.hs
+++ src/YesodDsl/Validation.hs
@@ -4,14 +4,12 @@ import YesodDsl.AST
 import Data.List
 import YesodDsl.Validation.Fields
 import YesodDsl.Validation.Handlers
-import qualified YesodDsl.Validation.State as V
 import Data.Maybe
 
 validate :: Module -> String
 validate m = fieldErrors m  
            ++ handlerErrors m
            ++ missingModuleName 
-           ++ (intercalate "\n" $ V.validate m)
     where 
           missingModuleName 
             | isNothing $ modName m = "Top-level module missing name"
